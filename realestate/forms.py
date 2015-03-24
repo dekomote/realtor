cb077466da867350ee5196d4beb72f19a73e2428
Dejan Noveski <dr.mote@gmail.com>
2015-03-24 08:07:46 +0100

    Point search

--- realestate/forms.py
+++ realestate/forms.py
@@ -18,18 +18,3 @@ class PolySearchForm(forms.Form):
             }
         },
         label = "", required = False,)
-
-class PointSearchForm(forms.Form):
-        poly = MapField(fields = [
-            EditableLayerField({'geometry': 'polygon', 'name': _('polygon')}),
-        ],
-        options = {
-            "default_lat": 41.6,
-            "default_lon": 22,
-            "default_zoom": 8,
-            "map_div_style": {
-                "width": "600px",
-                "height": "300px",
-            }
-        },
-        label = "", required = False,)
